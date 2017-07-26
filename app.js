d9913b5feb78001845670cc9f3670723eef2c3b2
jsrj <arjay.jones3301@gmail.com>
2017-07-26 14:21:14 -0400

    uncommented line that directs app to favicon

--- app.js
+++ app.js
@@ -29,8 +29,7 @@
   // default value for title local
   app.locals.title = 'ShopShare API | Usage Instructions';
 
-  // uncomment after placing your favicon in /public
-  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
